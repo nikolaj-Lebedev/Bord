294cc59f0f161764479be28f5f52e9a7b5930667
nikolaj-Lebedev <152249092+nikolaj-Lebedev@users.noreply.github.com>
2024-02-17 22:57:56 +0300

    Add files via upload

---
+++ scripts/tabs.js
@@ -0,0 +1,8 @@
+$(".reviews-switcher__link").click((e) => {
+    e.preventDefault();
+
+    const $this = $(e.currentTarget);
+    const curItem = $this.closest(".reviews-switcher__item");
+
+    curItem.addClass("active").siblings().removeClass("active");
+});
